86ae17e47b6ee956487c8eb7e4750040c4cc74b1
Shugailodasha <43368195+Shugailodasha@users.noreply.github.com>
2019-02-19 11:44:23 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,88 @@
-package practice2;
+package kontrol_work;
 
-import org.omg.CORBA.Object;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        User u1 = new User (2002, 5, 1, 23);
-        User u2 = new User(2003, 7,3,11);
-        System.out.println(u1.equals(u2));
+        Mother[] Mother = new Mother[3];
+        Mother[0] = new Mother(43,160);
+        Mother[1] = new Mother(23,178);
+        Mother[2] = new Mother(33,182);
+        Mother Test = (Mother[0].MakeDother());
+        ((Dother) Test).Sing();
+        if (Test instanceof Dother)
+        {
+            System.out.println("Test is Dother");
+        }
+        if (Test instanceof Dother)
+        {
+            System.out.println("Test is Son");
+        }
+    }
+    void Motherage (Object Mother)
+    {
+        System.out.println(((Mother) Mother).age);
     }
 }
-class User
+class Mother
 {
-    int ageYear;
-    int ageMounth;
-    int ageWeek;
-    int ageDay;
-    public User (int ageYear, int ageMounth, int ageWeek, int ageday) {
-        this.ageYear = ageYear;
-        this.ageMounth = ageMounth;
-        this.ageWeek = ageWeek;
-        this.ageDay = ageDay;
-    }
-   @Override
-   public boolean equals (Object obj) {
-        return (((User) obj).ageYear == this.ageYear&&
-                ((User)obj).ageMounth == this.ageMounth &&
-                ((User)obj).ageWeek == this.ageWeek &&
-                ((User)obj).ageDay == this.ageDay);
+ int age;
+ double height;
 
+ public Mother (int age, double height) {
+     this.age = age;
+     this.height = height;
+ }
+ Dother MakeDother()
+{
+    return new Dother(2, 120, false);
+}
+ Son MakeSon()
+ {
+   return new Son  (2, 160, true);
+ }
+
+     Dother d = new Dother(9,140,true ) ;
+     Son s = new Son( 17, 176, false);
+
+}
+class Dother extends Mother
+{
+ boolean isCanSing;
+ public Dother (int age, double height, boolean isCanSing){
+     super (age, height);
+     this.isCanSing = isCanSing;
+ }
+ void Sing()
+ {
+     if (isCanSing)
+     {
+         System.out.println("Sing in her room");
+     }
+     else
+     {
+         System.out.println("lerning to sing");
+     }
+
+ }
+}
+class Son extends Mother
+{
+ boolean isCanSing;
+ public Son (int age, double height, boolean isCanSing ){
+     super (age, height);
+     this.isCanSing = isCanSing;
+
+ }
+ void Sing (boolean isCanSing)
+ {
+   if (isCanSing)
+   {
+       System.out.println("Sing in his room");
    }
+   else
+       {
+           System.out.println("lerning to sing");
+       }
+ }
 }
